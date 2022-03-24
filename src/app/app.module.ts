c59c4422e0bd6484efe51a4ef02a3f4a84044eb1
Khamzat <xamzatseynaro@gmail.com>
2022-03-24 21:21:44 +0300

    add intercept provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Provider} from '@angular/core';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {registerLocaleData} from '@angular/common';
+import ruLocale from '@angular/common/locales/ru';
+import {ServiceWorkerModule} from '@angular/service-worker';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { PostPageComponent } from './post-page/post-page.component';
-import { PostComponent } from './shared/components/post/post.component';
-import { TestComponent } from './test/test.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {PostPageComponent} from './post-page/post-page.component';
+import {PostComponent} from './shared/components/post/post.component';
+import {SharedModule} from './shared/shared.module';
+import {AuthInterceptor} from './shared/auth.interceptor';
+import {environment} from '../environments/environment';
+
+
+registerLocaleData(ruLocale, 'ru');
+
+
+const INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  multi: true,
+  useClass: AuthInterceptor
+};
 
 @NgModule({
   declarations: [
@@ -15,14 +31,18 @@ import { TestComponent } from './test/test.component';
     MainLayoutComponent,
     HomePageComponent,
     PostPageComponent,
-    PostComponent,
-    TestComponent
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
   ],
-  providers: [],
+  providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+
