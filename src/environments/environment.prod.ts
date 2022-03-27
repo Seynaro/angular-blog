c36d752ee7c62de154074f5bebf4de595f9cd0f8
Khamzat <xamzatseynaro@gmail.com>
2022-03-27 10:17:33 +0300

    small refactoring

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyA55W-eNvkXaSiMR3lLEEwMSD0J-y0eiIo',
+  fbDbUrl: 'https://angular-blog-fda20-default-rtdb.firebaseio.com'
 };
